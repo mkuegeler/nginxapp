949bdf3d51d1608d2fdf003b7f9bf22d67943be2
Michael Kuegeler <mkuegeler@mac.com>
2020-09-08 12:23:26 +0200

    Update to nginx:alpine

--- app.sh
+++ app.sh
@@ -1,2 +1,20 @@
+# 1. Build image
 # docker build -t some-content-nginx .
-docker run --name some-nginx -d -p 8081:80 some-content-nginx
+
+# 2. Run container with local Dockerfile
+# docker run --name some-nginx -d -p 8081:80 some-content-nginx
+
+# 3. Run container with image from repo
+# docker run --name local-nginx -d -p 8081:80 mkuegeler/nginxapp:latest
+
+# 4. Open in browser
+# http://localhost:8081
+
+# 5. Stop container
+docker stop some-nginx 
+
+# 6. Remove container
+docker rm some-nginx
+
+# 7. Remove image
+docker rmi some-content-nginx
